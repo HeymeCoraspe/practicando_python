ff5bc3ec82476062c37a0dd698747527bd0c7472
Heyme <heyme.coraspe@gmail.com>
2024-08-16 20:24:35 -0300

    inicio ejercicios bucles

---
+++ otros_ejercicios/bucles.py
@@ -0,0 +1,20 @@
+#1 Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+'''
+palabra=input("Ingresa una palabra: ")
+
+for i in range(10):
+    print(palabra)'''
+
+#2 Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+'''
+edad= int(input("Ingresa tu edad: "))
+i=0
+for i in range (edad):
+    print(i+1)'''
+
+#3 Escribir un programa que pida al usuario un número entero positivo 
+# y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+
+
+
+
